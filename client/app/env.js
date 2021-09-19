67da21c477e063054f0f19237f2c960f7925f0d6
DariusDeah <dariusdeah21@gmail.com>
2021-09-19 14:08:02 -0600

    header and logo set

--- client/app/env.js
+++ client/app/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = ''
-export const audience = ''
-export const clientId = ''
+export const domain = 'demodays1.us.auth0.com'
+export const audience = 'https://student-auth.com'
+export const clientId = 'Y7tAnp3wlMh0iI1UUHRO25YgZLj1koKX'
